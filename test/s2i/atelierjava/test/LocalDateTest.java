2695dd12ddc29b754ebbfbed2f09c7403330d13d
quicotte-formation <quicotte@gmail.com>
2020-04-30 12:43:17 +0200

    LocalDate

--- test/s2i/atelierjava/test/LocalDateTest.java
+++ test/s2i/atelierjava/test/LocalDateTest.java
@@ -5,6 +5,9 @@
  */
 package s2i.atelierjava.test;
 
+import static org.testng.Assert.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import org.testng.annotations.Test;
 
 /**
@@ -16,10 +19,22 @@ public class LocalDateTest {
     @Test
     public void convertir_30_04_2020_vers_2020_04_30OK(){
         
+        DateTimeFormatter f1 = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter f2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        
+        LocalDate ld = LocalDate.parse("30-04-2020", f1);
+        String texte = ld.format(f2);
+        assertEquals(texte, "2020-04-30");
     }
     
     @Test
     public void ajouterUnAnUnMoisUnJourADateDuJourOK(){
         
+        LocalDate now = LocalDate.now();
+        now = now.plusYears(1);
+        now = now.plusMonths(1);
+        now = now.plusDays(1);
+        
+        System.out.println( now );
     }
 }
