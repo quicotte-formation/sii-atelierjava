35ee658e3b3365b82747dca5a8e0710b01ea3f94
quicotte-formation <quicotte@gmail.com>
2020-05-05 14:33:33 +0200

    Animaltest

--- test/s2i/atelierjava/test/heritage/AnimalTest.java
+++ test/s2i/atelierjava/test/heritage/AnimalTest.java
@@ -5,8 +5,11 @@
  */
 package s2i.atelierjava.test.heritage;
 
+import java.util.ArrayList;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
+import s2i.atelierjava.heritage.Animal;
+import s2i.atelierjava.heritage.FlamandRose;
 import s2i.atelierjava.heritage.Oiseau;
 
 /**
@@ -16,6 +19,18 @@ import s2i.atelierjava.heritage.Oiseau;
 public class AnimalTest {
     
     @Test
+    public void genericite(){
+        Animal a = new FlamandRose();// = GéNERICITé
+        a.avancer();
+        System.out.println( a.toString() );
+        
+        //
+        
+        ArrayList<Animal> animaux = new ArrayList<>();
+        animaux.add(new FlamandRose());
+    }
+    
+//    @Test
     public void testAvanceOiseau(){
         Oiseau o = new Oiseau();
         o.avancer();
